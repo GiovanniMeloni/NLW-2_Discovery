cab6372238857f6c01db5e714109357742584e98
Giovanni-P-Meloni <gio.mel@hotmail.com>
2020-08-07 12:55:33 -0300

    NLW#2 feita

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require("path")
 const express = require("express")
 const server = express()
 const nunjucks = require("nunjucks")
-const { pageLanding, pageStudy, pageGiveClasses } = require("./pages")
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require("./pages")
 
 //Nunjucks configure
 nunjucks.configure('src/views', {
@@ -13,11 +13,14 @@ nunjucks.configure('src/views', {
 
 //Server
 server
+//receber os dados do req body
+.use(express.urlencoded( {extended:true} ) )
 //configurar arquivos estaticos
 .use(express.static("public"))
 //rotas da app
 .get("/", pageLanding)  
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 //start so server
 .listen(5000)
